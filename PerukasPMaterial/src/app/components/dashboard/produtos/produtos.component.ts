61d8f0bc7bee398f519738319d15102f240daddb
BootSectorAlpha <lutiell@hotmail.com>
2022-06-21 15:44:54 -0300

    update produtos e usuarios .ts

--- PerukasPMaterial/src/app/components/dashboard/produtos/produtos.component.ts
+++ PerukasPMaterial/src/app/components/dashboard/produtos/produtos.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Produtos } from 'src/app/interfaces/produtos';
-
-const listarProdutos: Produtos[] = [
-  {nome: 'Impressora', valor: 250, imagem : new Image() }
-
-];
 
 @Component({
   selector: 'app-produtos',
@@ -12,12 +8,21 @@ const listarProdutos: Produtos[] = [
   styleUrls: ['./produtos.component.css']
 })
 export class ProdutosComponent implements OnInit {
+
+  listarProdutos: Produtos[] = [
+    {nome: 'Impressora', valor: 250, imagem : new Image() }
+  ];
+
   displayedColumns: string[] = ['nome', 'valor', 'imagem', 'acaoProduto'];
-  dataSource = listarProdutos;
+  dataSource = new MatTableDataSource(this.listarProdutos);
   constructor() { }
 
-
   ngOnInit(): void {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
